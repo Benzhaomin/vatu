6b4f2bb14177733ea9727d104ccff8101c8ee9c2
Ben <ben@wainei.net>
2017-09-24 20:13:22 +0200

    use isoformat for datetime objects

--- vatu/db.py
+++ vatu/db.py
@@ -43,42 +43,21 @@ class DB:
 os.makedirs(DB.PATH, exist_ok=True)
 
 
-# JSON dates encoder and decoder from https://gist.github.com/abhinav-upadhyay/5300137
-
 class DateTimeDecoder(json.JSONDecoder):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, object_hook=self.dict_to_object, **kwargs)
 
-    def dict_to_object(self, d):
-        if '__type__' not in d:
-            return d
-
-        _type = d.pop('__type__')
-        try:
-            dateobj = datetime.datetime(**d)
-            return dateobj
-        except:
-            d['__type__'] = _type
-            return d
+    def dict_to_object(self, json_dict):
+        for (key, value) in json_dict.items():
+            try:
+                json_dict[key] = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            except Exception:
+                pass
+        return json_dict
 
 
 class DateTimeEncoder(json.JSONEncoder):
-    """ Instead of letting the default encoder convert datetime to string,
-        convert datetime objects into a dict, which can be decoded by the
-        DateTimeDecoder
-    """
-
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
-            return {
-                '__type__': 'datetime',
-                'year': obj.year,
-                'month': obj.month,
-                'day': obj.day,
-                'hour': obj.hour,
-                'minute': obj.minute,
-                'second': obj.second,
-                'microsecond': obj.microsecond,
-            }
-        else:
-            return json.JSONEncoder.default(self, obj)
+            return obj.isoformat()
+        return json.JSONEncoder.default(self, obj)
